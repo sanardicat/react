d2bff022af660203af4083ba7442534d210e970d
Cat <azam.analytics@gmail.com>
2023-10-07 16:46:30 +0100

    routing

--- src/Components/Listings.js
+++ src/Components/Listings.js
@@ -5,7 +5,7 @@ function Listings() {
     <div>
         <h1>This is the Listings page</h1>
         <br />
-        <Link to='/login'>Login</a>
+        <Link to='/login'>Login</Link>
         <a href='/'>Home</a>
     </div>
   )
